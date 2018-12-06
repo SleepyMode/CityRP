87768d7bb44e3a3d8871408df38d290ca30e52c8
zoephix <zep@heijmans.us>
2018-12-06 23:08:40 +0100

    Playerspawn Walkspeed

--- gamemode/sh_init.lua
+++ gamemode/sh_init.lua
@@ -7,6 +7,23 @@ PLAYER = FindMetaTable("Player")
 ENTITY = FindMetaTable("Entity")
 
 --[[-------------------------------------------------------------------------
+PlayerSpawn
+---------------------------------------------------------------------------]]
+
+
+hook.Add( "PlayerSpawn", "PlayerSpawn", function( ply )
+
+    timer.Simple(0.1, function()
+        if !IsValid(ply) then return end
+
+        ply:SetWalkSpeed(135)
+        ply:SetRunSpeed(200)
+
+    end)
+end)
+
+
+--[[-------------------------------------------------------------------------
 Module Library
 ---------------------------------------------------------------------------]]
 
