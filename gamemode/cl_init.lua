15b0b005c2a1ca595f7f1e4cc9cf474ba890f17f
SleepyMode <nivnuv10@gmail.com>
2018-12-07 12:46:23 +0200

    Undo @zoephix - put it in an hud module, don't fuck everything up.

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,8 +1,2 @@
+
 include("sh_init.lua")
-
-hook.Add( "HUDShouldDraw", "Hide_Hud", function( name )
-if ( name == "CHudHealth" or name == "CHudBattery" ) then
-return false
-end
-
-end )
--- gamemode/sh_init.lua
+++ gamemode/sh_init.lua
@@ -6,22 +6,6 @@ GM.Author = "Zoephix & SleepyMode"
 PLAYER = FindMetaTable("Player")
 ENTITY = FindMetaTable("Entity")
 
---[[-------------------------------------------------------------------------
-PlayerSpawn
----------------------------------------------------------------------------]]
-
-
-hook.Add( "PlayerSpawn", "PlayerSpawn", function( ply )
-
-    timer.Simple(0.1, function()
-        if !IsValid(ply) then return end
-
-        ply:SetWalkSpeed(135)
-        ply:SetRunSpeed(200)
-
-    end)
-end)
-
 
 --[[-------------------------------------------------------------------------
 Module Library
