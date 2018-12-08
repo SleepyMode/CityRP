8b69258d76c356e6228b42485806fd3c282e177c
SleepyMode <nivnuv10@gmail.com>
2018-12-08 11:25:23 +0200

    Fix animations sh_module

--- gamemode/modules/animations/sh_module.lua
+++ gamemode/modules/animations/sh_module.lua
@@ -1,9 +1,11 @@
+
 if (SERVER) then
 	AddCSLuaFile("cl_animations.lua")
 	AddCSLuaFile("sh_animations.lua")
-	AddCSLuaFile("sv_animations.lua")
 
-	include("cl_animations.lua")
 	include("sh_animations.lua")
 	include("sv_animations.lua")
+else
+	include("sh_animations.lua")
+	include("cl_animations.lua")
 end
